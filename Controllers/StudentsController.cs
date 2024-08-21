97c0152e9608c60fe9184e3ad129a0980f7bcff2
Raghad-Moqady <raghadmoqady@gmail.com>
2024-08-21 19:17:34 +0300

    controller, view(header&main&footer)

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task1.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult GetAll()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+
+    }
+}
